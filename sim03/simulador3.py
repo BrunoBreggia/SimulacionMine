b5c82f436e7a2318e1157282d9d86d4944763bf2
BrunoBreggia <breggiabruno@gmail.com>
2023-06-13 18:47:35 -0300

    Creacion de archivo adicional con el valor de todas las variables iterables de la simulacion

--- sim03/simulador3.py
+++ sim03/simulador3.py
@@ -9,7 +9,7 @@ Se usará la implementación tradicional de MINE (Mutual information neural esti
 [SimConfig]
 Sim_filename='Exp_03'
 Sim_variables={'RHO_IDX':[0,1,2],'ACT_IDX':[0,1,2]}
-Sim_realizations={'REA':1}
+Sim_realizations={'DUMMY':1}
 Sim_name='E03'
 Sim_hostname='cluster-fiuner'
 [endSimConfig]
@@ -17,8 +17,8 @@ Sim_hostname='cluster-fiuner'
 #SBATCH --mail-user=bruno.breggia@uner.edu.ar
 #SBATCH --partition=internos
 #SBATCH --nodes=1
-#SBATCH --ntasks=48
-#SBATCH --tasks-per-node=48
+#SBATCH --ntasks=24
+#SBATCH --tasks-per-node=24
 [endSlurmConfig]
 '''
 import numpy as np
@@ -29,38 +29,38 @@ import time
 import pandas as pd
 from datetime import datetime
 from mine.mine2 import Mine2
-from copy import deepcopy
 
 # ############# variables de simulacion  ##############
+# Iterables de la simulacion
+rhos = [0.0, 0.5, 0.98]  # Fijado por simulacion
+act_funcs = ["relu", "Lrelu", "elu"]  # Fijado por simulacion
+valores_capas = [1, 2, 3]
+valores_neuronas = [50, 100, 200]
+valores_muestras = [1e3, 3e3, 5e3, 10e3]
+
 # Indices fijados por simconfig
 RHO_IDX = 0
 ACT_IDX = 0
-REA = 1
-subREA = 2
-# TODO: preguntar a Feli que eran los subREA
+DUMMY = 1
+REA = 24
 
-# variables de archivo
-RHO = [0.0, 0.5, 0.98][RHO_IDX]
-ACT_FUNC = ["relu", "Lrelu", "elu"][ACT_IDX]
+# Constantes de archivo
+RHO = rhos[RHO_IDX]
+ACT_FUNC = act_funcs[ACT_IDX]
 LR = 1e-3  # 0.5
-TRAIN_PERCENT = 0.8
-MINIBATCH_PERCENT = 0.10  # porcenaje respecto del total del dataset
+TRAIN_PERCENT = 80  # porcentaje respecto del total del dataset
+MINIBATCH_PERCENT = 0.10  # porcentaje respecto del total del dataset
 MAX_EPOCAS = 15_000
 LR_PATIENCE = 250
 LR_FACTOR = 0.5
 VALIDATION_AVG = 100
 STOP_PATIENCE = 1000
 
-# variables a iterar en archivo
-valores_capas = [1, 2, 3]
-valores_neuronas = [50, 100, 200]
-valores_muestras = [1e3, 3e3, 5e3, 10e3]
-
-# donde va a correr la simulacion
+# Donde va a correr la simulacion
 cuda = "cuda:0" if torch.cuda.is_available() else "cpu"
 # cuda = "cpu"
 
-# directorio con resultados
+# Directorio con resultados
 sim = "sim03"
 path = os.getcwd()
 OUTDIR = path + "/outData"
@@ -69,7 +69,7 @@ if not os.path.isdir(OUTDIR):
     print("created folder : ", OUTDIR, flush=True)
 else:
     print(OUTDIR, "folder already exists.", flush=True)
-output_file = f"{OUTDIR}/{sim}_R{RHO_IDX}_A{ACT_IDX}_R{subREA}.csv"  # TODO: por que aparece subREA aca
+output_file = f"{OUTDIR}/{sim}_R{RHO_IDX}_A{ACT_IDX}_R{REA}.csv"
 
 # Diccionario para los datos de cada realizacion -> convertir a dataframe
 data = {}
@@ -106,7 +106,7 @@ def signal_generator(mean=(0, 0), correlation_rho=0.5, samples=1000):
     return (x, z), true_mi
 
 # ############## inicializo ray ##############
-# ray.init(num_cpus=subREA)
+# ray.init(num_cpus=REA)
 
 
 # @ray.remote
@@ -114,26 +114,34 @@ def correr_epocas(red: Mine2, samples: int):
     dataLocal = {}
 
     (x, z), true_mi = signal_generator(correlation_rho=RHO, samples=samples)
-    minibatch_size = samples * MINIBATCH_PERCENT
+    minibatch_size = int(samples * MINIBATCH_PERCENT)
 
     red.fit(x, z, train_percent=TRAIN_PERCENT, minibatch_size=minibatch_size, learning_rate=LR,
             num_epochs=MAX_EPOCAS, random_partition=True, patience=LR_PATIENCE, scaling_factor=LR_FACTOR)
 
     estimador1, estimador2, estimador3 = red.estimacion_mi()
 
+    # # Almacenamiento de datos de la realizacion # #
+    # Parametros de la señal
     dataLocal["rho"] = RHO
     dataLocal["true_mi"] = true_mi
     dataLocal["samples"] = samples
+
+    # Parametros constructivos de la red
+    dataLocal["capas"] = red.hiddenLayers
+    dataLocal["neuronas"] = red.neurons
+    dataLocal["funcion_activacion"] = ACT_FUNC
+
+    # Parametros de entrenamiento
     dataLocal["LR"] = LR
     dataLocal["LR_patience"] = LR_PATIENCE
     dataLocal["LR_factor"] = LR_FACTOR
-    dataLocal["capas"] = red.hiddenLayers
-    dataLocal["neuronas"] = red.neurons
     dataLocal["minibatch_size"] = minibatch_size
     dataLocal["last_epoch"] = red.last_epoc
     dataLocal["validation_avg"] = VALIDATION_AVG
     dataLocal["stop_patience"] = STOP_PATIENCE
 
+    # Resultados
     dataLocal["estimador1"] = estimador1[0]
     dataLocal["estimador1_epoca"] = estimador1[1]
     dataLocal["estimador2"] = estimador2[0]
@@ -152,7 +160,7 @@ def main():
     for index1, neuronas in enumerate(valores_neuronas):
         for index2, capas in enumerate(valores_capas):
             for index3, samples in enumerate(valores_muestras):
-                for _ in range(subREA):
+                for _ in range(REA):
                     mines.append(Mine2(capas, neuronas, ACT_FUNC,
                                        validation_average=VALIDATION_AVG, stop_patience=STOP_PATIENCE))
                 realizaciones = [correr_epocas(mine, int(samples)) for mine in mines]
@@ -173,7 +181,25 @@ def main():
     data_df.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
 
 
+def generate_aux_datafile():
+    sim_iterables = {
+        "rhos": rhos,
+        "act_funcs": act_funcs,
+        "valores_capas": valores_capas,
+        "valores_neuronas": valores_neuronas,
+        "valores_muestras": valores_muestras,
+    }
+    auxFile_name = f"{OUTDIR}/sim_iterables.txt"
+    if not os.path.exists(auxFile_name):
+        with open(auxFile_name, 'w', encoding='utf-8') as auxFile:
+            for key in sim_iterables.keys():
+                auxFile.write(f"{key} : {sim_iterables[key]}")
+
+
 if __name__ == '__main__':
+
+    generate_aux_datafile()
+
     inicio = datetime.now()
     print("inicio:", inicio, flush=True)
 
